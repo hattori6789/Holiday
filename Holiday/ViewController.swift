976f8a038e4e812d567271e5ce53d8492240a41d
hattori6789 <grgbgrg6789@gmail.com>
2016-06-27 21:26:30 +0900

    second commit

--- Holiday/ViewController.swift
+++ Holiday/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // hamuhamu
+        // second commit
     }
 
     override func didReceiveMemoryWarning() {
